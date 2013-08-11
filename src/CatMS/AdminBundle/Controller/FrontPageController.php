7aa2e50802b67d1535d5108efda93c206becd89c
Dawid Job <d.job@catdesign.pl>
2013-08-11 14:40:21 +0200

    Locale translations bone

--- src/CatMS/AdminBundle/Controller/FrontPageController.php
+++ src/CatMS/AdminBundle/Controller/FrontPageController.php
@@ -5,6 +5,7 @@ namespace CatMS\AdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Admin front page controller.
@@ -18,12 +19,15 @@ class FrontPageController extends Controller
      */
     public function indexAction()
     {
-        $request = $this->getRequest();
-        
-        //var_dump($request->getHttpHost());
-        
         return $this->render('CatMSAdminBundle:FrontPage:index.html.twig', 
             array()
         );
+    }
+    
+    public function setCurrentLocaleAction($locale)
+    {
+        return new Response(json_encode(array('result' => 'success')), 200, 
+            array('Content-Type' => 'application/json')
+        ); 
     }
 }
